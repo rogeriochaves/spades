49568de005f4e7944870f1019f27f272fd74baa5
Rogerio Chaves <rogeriocfj@gmail.com>
2018-06-07 19:01:02 -0300

    Remove webpack, it is not needed

--- generate/src/index.js
+++ generate/src/index.js
@@ -1,7 +1,8 @@
-import { JSDOM } from "jsdom";
-import { Script } from "vm";
-import Elm from "raw-loader!./Main.elm";
-import ElmInterface from "raw-loader!./elm-interface";
+const { JSDOM } = require("jsdom");
+const { Script } = require("vm");
+const fs = require("fs");
+const Elm = fs.readFileSync(`${__dirname}/../out/elm.js`);
+const ElmInterface = fs.readFileSync(`${__dirname}/elm-interface.js`);
 
 const dom = new JSDOM(`<!DOCTYPE html><html><head><body></body></html>`, {
   runScripts: "outside-only"
