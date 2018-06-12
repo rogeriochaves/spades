4c9dc899f47697c97985b888122d107b99626cfe
Rogerio Chaves <rogeriocfj@gmail.com>
2018-06-11 23:28:28 -0300

    Server side render using the built bundle when in production mode

--- boilerplate/server.js
+++ boilerplate/server.js
@@ -1,36 +1,52 @@
 const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackConfig = require("./webpack.config");
 const express = require("express");
+const fs = require("fs");
 const { JSDOM } = require("jsdom");
 const { Script } = require("vm");
 
 const app = express();
-const compiler = webpack({ ...webpackConfig, mode: "production" });
-const webpackMiddleware = webpackDevMiddleware(compiler, {
-  serverSideRender: true
-});
-
 app.set("view engine", "ejs");
 app.set("views", "./src");
-app.use(webpackMiddleware);
+app.use(express.static("build"));
+
+let webpackMiddleware;
+if (process.env.NODE_ENV !== "production") {
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackConfig = require("./webpack.config");
+  const compiler = webpack({ ...webpackConfig, mode: "development" });
+  webpackMiddleware = webpackDevMiddleware(compiler, {
+    serverSideRender: true,
+    publicPath: "/"
+  });
+  app.use(webpackMiddleware);
+}
 
 app.use((req, res, next) => {
-  const assetsByChunkName = res.locals.webpackStats.toJson().assetsByChunkName;
-  const bundlePath = assetsByChunkName.main;
-  const bundleFile = webpackMiddleware.fileSystem.readFileSync(
-    `/${process.cwd()}/build/${bundlePath}`,
-    "utf8"
-  );
+  const bundle = getBundle(res);
   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-  console.log("fullUrl", fullUrl);
 
-  renderElmApp(bundleFile, fullUrl)
+  renderElmApp(bundle.file, fullUrl)
     .then(renderedHtml => {
-      res.render("index", { bundlePath, renderedHtml });
+      res.render("index", { bundlePath: bundle.path, renderedHtml });
     })
     .catch(next);
 });
+
+const getBundle = res => {
+  let path;
+  let file;
+  if (process.env.NODE_ENV === "production") {
+    path = require("./build/stats.json").assetsByChunkName.main;
+    file = fs.readFileSync(`./build/${path}`, "utf8");
+  } else {
+    path = res.locals.webpackStats.toJson().assetsByChunkName.main;
+    file = webpackMiddleware.fileSystem.readFileSync(
+      `/${process.cwd()}/build/${path}`,
+      "utf8"
+    );
+  }
+  return { path, file };
+};
 
 const renderElmApp = (bundleFile, url) =>
   new Promise((resolve, reject) => {
