5748e80286b4361d28a91a4bcd6a488d75a2ea47
W. Brian Gourlie <bgourlie@gmail.com>
2018-09-30 21:06:22 -0500

    Fix running tests on Windows

--- tests/functional/createApp.test.js
+++ tests/functional/createApp.test.js
@@ -1,30 +1,30 @@
 const { run } = require("./helpers");
-const fs = require("fs");
+const fs = require("fs-extra");
 
 const ONE_MINUTE = 60 * 1000;
 
 test(
   "creates a new app with the same files of the boilerplate except the ignored folders",
-  () =>
-    run(`npm -g install .`)()
-      .then(run("rm -rf temp; mkdir temp"))
-      .then(run("cd temp && elm-generate app myapp"))
-      .then(() => {
-        const newAppFiles = fs.readdirSync("temp/myapp");
-        const ignoredFiles = fs
+  async () => {
+      await run("npm -g install .")();
+      await fs.remove("temp");
+      await fs.ensureDir("temp");
+      await run("cd temp && elm-generate app myapp")();
+      const newAppFiles = fs.readdirSync("temp/myapp");
+      const ignoredFiles = fs
           .readFileSync("boilerplate/.gitignore.template")
           .toString("utf-8")
           .split("\n");
-        ignoredFiles.push(".gitignore.template");
-        ignoredFiles.push(".DS_Store");
+      ignoredFiles.push(".gitignore.template");
+      ignoredFiles.push(".DS_Store");
 
-        let boilerplateFiles = fs
+      let boilerplateFiles = fs
           .readdirSync("boilerplate")
           .filter(folder => !ignoredFiles.includes(folder));
-        boilerplateFiles = [".gitignore", ...boilerplateFiles];
+      boilerplateFiles = [".gitignore", ...boilerplateFiles];
 
-        expect(newAppFiles).toEqual(boilerplateFiles);
-      }),
+      expect(newAppFiles).toEqual(boilerplateFiles);
+  },
   ONE_MINUTE
 );
 
