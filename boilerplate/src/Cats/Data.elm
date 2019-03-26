cbdbbd94f4182fec52c098681702646bbf34bdae
Matheus Ashton Silva <matheusashton@gmail.com>
2019-03-25 22:30:47 -0300

    update http call
    updated http call, changed `RemoteData.sendRequest` that was removed to the new `Http.get` call from `elm/http@2.0.0`

--- boilerplate/src/Cats/Data.elm
+++ boilerplate/src/Cats/Data.elm
@@ -11,10 +11,14 @@ getRandomGif topic =
     let
         url =
             "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+        returnMsg =
+            RemoteData.fromResult >> NewGif
     in
-    Http.get url decodeGifUrl
-        |> RemoteData.sendRequest
-        |> Cmd.map NewGif
+    Http.get
+        { url = url
+        ,   expect = Http.expectJson returnMsg decodeGifUrl
+        }
 
 
 decodeGifUrl : Decode.Decoder String
